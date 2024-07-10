81972e4e417cdb75de1e4fa5cb0ee8ba74192258
yogam00 <yogesh@seedlegals.com>
2024-07-10 10:41:51 +0100

    first commit

---
+++ auto.sh
@@ -0,0 +1,5 @@
+kubectl create namespace argocd
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
+kubectl port-forward svc/argocd-server -n argocd 8181:443
+kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
