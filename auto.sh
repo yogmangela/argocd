50f0fbe4831e2dd77655affca36baece7b32e4d8
yogam00 <yogesh@seedlegals.com>
2024-07-10 11:14:53 +0100

    create namespace airbyte

--- auto.sh
+++ auto.sh
@@ -3,3 +3,5 @@ kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/st
 
 kubectl port-forward svc/argocd-server -n argocd 8181:443
 kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d
+
+kubectl create namespace airbyte
